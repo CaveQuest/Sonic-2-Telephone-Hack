edc9e036fd3c627e89b493aa0ff53d4b8258b8c2
Clownacy <Clownacy@users.noreply.github.com>
2016-05-30 18:29:52 +0100

    Extracted resources from RAR file
    Commit history is a better way of logging changes

---
+++ s2.h
@@ -0,0 +1,7 @@
+comp_z80_size 0xF64 e fÅr C-Programm */
+#define word_728C_user 0x71A8
+#define Obj5F_MapUnc_7240 0x7228
+#define off_3A294 0x3A27C
+#define MapRUnc_Sonic 0x71480
+#define movewZ80CompSize 0xEC04E
+/* Ende Includefile fÅr C-Programm */
